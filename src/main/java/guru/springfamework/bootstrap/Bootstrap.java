87ae560b96c60a2c15a43087c31d28b5912b7d67
isabelbenavidez <isabelbenavidez@gmail.com>
2022-09-08 15:23:35 -0500

    Se crea API REST para CUSTOMER , incluyendo todas las capas, Se hace prueba en POSTMAN y sí funciona. Solo faltan las pruebas

--- src/main/java/guru/springfamework/bootstrap/Bootstrap.java
+++ src/main/java/guru/springfamework/bootstrap/Bootstrap.java
@@ -1,47 +1,77 @@
 package guru.springfamework.bootstrap;
 
 import guru.springfamework.domain.Category;
+import guru.springfamework.domain.Customer;
 import guru.springfamework.repositories.CategoryRepository;
+import guru.springfamework.repositories.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
-import java.sql.SQLOutput;
 
 @Component
 public class Bootstrap implements CommandLineRunner { //CommandLineRunner es un motor de arranque, permite ejecutar el código de la aplicacio
 
     private CategoryRepository categoryRepository;
+    private CustomerRepository customerRepository;
 
 
-    public Bootstrap(CategoryRepository categoryRepository) {
+    public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository) {
         this.categoryRepository = categoryRepository;
+        this.customerRepository = customerRepository;
     }
 
 
     @Override
     public void run(String... args) throws Exception {
 
-        Category fruits = new Category();
-        fruits.setName("Fruits");
+        loadCategories();
+        loadCustomers();
+    }
+        private void loadCategories() {
 
-        Category dried = new Category();
-        dried.setName("Dried");
+            Category fruits = new Category();
+            fruits.setName("Fruits");
 
-        Category fresh = new Category();
-        fresh.setName("Fresh");
+            Category dried = new Category();
+            dried.setName("Dried");
 
-        Category exotic = new Category();
-        exotic.setName("Exotic");
+            Category fresh = new Category();
+            fresh.setName("Fresh");
 
-        Category nuts = new Category();
-        nuts.setName("Nuts");
+            Category exotic = new Category();
+            exotic.setName("Exotic");
 
-        categoryRepository.save(fruits);
-        categoryRepository.save(dried);
-        categoryRepository.save(fresh);
-        categoryRepository.save(exotic);
-        categoryRepository.save(nuts);
+            Category nuts = new Category();
+            nuts.setName("Nuts");
 
-        System.out.println("Datos cargados = " +categoryRepository.count());
+            categoryRepository.save(fruits);
+            categoryRepository.save(dried);
+            categoryRepository.save(fresh);
+            categoryRepository.save(exotic);
+            categoryRepository.save(nuts);
+
+            System.out.println("Datos cargados = " + categoryRepository.count());
+        }
+
+        private void loadCustomers(){
+
+        //given
+            Customer customer1 = new Customer();
+            customer1.setId(1l);
+            customer1.setFirstName("César");
+            customer1.setLastName("Tenganán");
+            customerRepository.save(customer1);
+
+
+            Customer customer2 = new Customer();
+            customer2.setId(2l);
+            customer2.setFirstName("Pites");
+            customer2.setLastName("Benavides");
+
+            customerRepository.save(customer2);
+
+            System.out.println("Customers cargados: " + customerRepository.count());
+
+
+
     }
 }
